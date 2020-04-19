261a7ba547371e26854aca04f93bfdf5c1c73ca1
wentao <569845499@qq.com>
2020-04-19 18:15:10 +0800

    add food module

---
+++ greedy snake/js/food.js
@@ -0,0 +1,47 @@
+(function(window, undefined){
+
+    var elementsFood = []; // only has one element: previously created food div 
+
+    function Food(x, y){
+        this.x = x || 0;
+        this.y = y || 0;
+    }
+
+    function removeFood(){
+        elementsFood[0].parentNode.removeChild(elementsFood[0]);
+        elementsFood.pop();
+    }
+
+    // render food object to map
+    Food.prototype.renderFood = function(map){
+        // remove previous food
+        if(elementsFood[0]) {removeFood()};
+
+        // generate random positon of food in map
+        this.x = parseInt(map.offsetWidth / 20 * Math.random()) * 20;
+        this.y = parseInt(map.offsetHeight / 20 * Math.random()) * 20;
+        
+        // create food div and set style
+        var div = document.createElement('div');
+        map.appendChild(div);
+        elementsFood.push(div);
+
+        div.setAttribute('class', 'food');
+
+        div.style.left = this.x + 'px';
+        div.style.top = this.y + 'px';
+        
+        
+    };
+    
+    // test
+    var map = document.getElementById('map');
+    var food = new Food();
+    food.renderFood(map);
+
+
+    // expose Food to other modules
+    window.Food = Food;
+
+})(window, undefined);
+
